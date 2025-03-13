62327a9dfa2745810411449f33de10a55470691a
akiraonstarknet <zkdice@proton.me>
2025-03-13 17:19:57 +0530

    add open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,25 @@ export const metadata: Metadata = {
   title: 'Starknet Will Melt Faces | SWMF',
   description: 'Starknet to be the first L2 to settle on both Bitcoin and Ethereum',
   icons: '/favicon.ico',
+  openGraph: {
+    title: 'Starknet Will Melt Faces | SWMF',
+    description: 'Starknet to be the first L2 to settle on both Bitcoin and Ethereum',
+    images: [
+      {
+        url: 'https://swmf.fun/sample-message.png',
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Starknet Will Melt Faces | SWMF',
+    description: 'Starknet to be the first L2 to settle on both Bitcoin and Ethereum',
+    images: [
+      {
+        url: 'https://swmf.fun/sample-message.png',
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
